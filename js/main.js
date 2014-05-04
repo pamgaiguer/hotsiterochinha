a4a19feb4fbf45ac12cf7c79bf2672f2a3dbb6a6
Pam Gaiguer <pamella.gaiguer@gmail.com>
2014-05-04 00:54:12 -0300

    inserindo js de scroll na pagina

---
+++ js/main.js
@@ -0,0 +1,97 @@
+$(document).ready(function() {
+	var goTop = $('<div class="go-top">Voltar ao topo</div>');
+	$('body').append(goTop);
+	$(window).scroll(function() {
+		if ($(this).scrollTop() != 0) {
+			goTop.fadeIn();
+		} else {
+			goTop.fadeOut();
+		}
+	});
+	goTop.click(function() {
+		$('body,html').animate({
+			scrollTop : 0
+			}, 800);
+	});
+	
+	//link para eventos
+	$('a[href^="#bg2"]').on('click',function (e) {
+	    e.preventDefault();
+
+	    var target = this.hash,
+	    $target = $(target);
+
+	    $('html, body').stop().animate({
+	        'scrollTop': $target.offset().top
+	    }, 900, 'swing', function () {
+	        window.location.hash = target;
+	    });
+	});
+	//link para pacotes
+	$('a[href^="#bg3"]').on('click',function (e) {
+	    e.preventDefault();
+
+	    var target = this.hash,
+	    $target = $(target);
+
+	    $('html, body').stop().animate({
+	        'scrollTop': $target.offset().top
+	    }, 900, 'swing', function () {
+	        window.location.hash = target;
+	    });
+	});
+	//link para personalizados
+	$('a[href^="#bg4"]').on('click',function (e) {
+	    e.preventDefault();
+
+	    var target = this.hash,
+	    $target = $(target);
+
+	    $('html, body').stop().animate({
+	        'scrollTop': $target.offset().top
+	    }, 900, 'swing', function () {
+	        window.location.hash = target;
+	    });
+	});
+	//link para sabores
+	$('a[href^="#bg5"]').on('click',function (e) {
+	    e.preventDefault();
+
+	    var target = this.hash,
+	    $target = $(target);
+
+	    $('html, body').stop().animate({
+	        'scrollTop': $target.offset().top
+	    }, 900, 'swing', function () {
+	        window.location.hash = target;
+	    });
+	});
+	//link para pedidos
+	$('a[href^="#bg6"]').on('click',function (e) {
+	    e.preventDefault();
+
+	    var target = this.hash,
+	    $target = $(target);
+
+	    $('html, body').stop().animate({
+	        'scrollTop': $target.offset().top
+	    }, 900, 'swing', function () {
+	        window.location.hash = target;
+	    });
+	});
+	//link para pedidos
+	$('a[href^="#bg7"]').on('click',function (e) {
+	    e.preventDefault();
+
+	    var target = this.hash,
+	    $target = $(target);
+
+	    $('html, body').stop().animate({
+	        'scrollTop': $target.offset().top
+	    }, 900, 'swing', function () {
+	        window.location.hash = target;
+	    });
+	});					
+	
+	
+});
