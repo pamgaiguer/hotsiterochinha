b10bd39a485869e48334f09836a8188b08ee6380
Pam Gaiguer <pamella.gaiguer@gmail.com>
2014-05-16 01:17:44 -0300

    novo arquivo

---
+++ index.php
@@ -0,0 +1,170 @@
+<!DOCTYPE html>
+<html lang="pt">
+	<head>
+		<meta charset="UTF-8" />
+		<meta http-equiv="X-UA-Compatible" content="IE=EmulateIE10" />
+		<meta name="viewport" content="width=device-width">
+		<title>Rochinha Eventos</title>
+		<meta name="description" content="">
+		<meta name="author" content="Pamella Gaiguer">
+		<link rel="shortcut icon" href="/favicon.ico">
+		<link rel="stylesheet" href="css/bootstrap.min.css">
+		<link rel="stylesheet" href="css/typografy.css">
+		<link rel="stylesheet" href="css/main.css">
+		<link rel="stylesheet" href="css/estilos.css">		
+	</head>
+	<body>
+		<div class="container">
+			<div class="row">
+				<div class="col-md-12">
+					<div id="topo">
+						<img src="images/logo-rochinha-titulo.png" class="img-responsive" id="logo-topo">
+						<div class="botoes-topo">
+							<a href="#eventos"><img src="images/botao-topo-001-eventos.png"></a>
+							<a href="#pacotes"><img src="images/botao-topo-002-pacotes.png"></a>
+							<a href="#personalizados"><img src="images/botao-topo-003-personalizados.png"></a>
+							<a href="#sabores"><img src="images/botao-topo-004-sabores.png"></a>
+							<a href="#duvidas"><img src="images/botao-topo-005-duvidas.png"></a>
+							<a href="#pedidos"><img src="images/botao-topo-006-pedidos.png"></a>
+						</div>
+					</div>
+					<div id="eventos"> 
+						<div id="eventos-text" class="container-textos">
+							<p class="texto01">Você sabia que sua festa pode ter um</p> 
+								<p class="texto02">carrinho recheado com picolés Rochinha?</p>
+								<br><br>
+							<p class="texto03">Disponibilizamos carrinhos e freezers</p>
+								<p class="texto04">para vários tipos de eventos</p>
+							<ul class="list-events left">
+								<li><img src="images/icon-aniversario.png">Aniversário</li>
+								<li><img src="images/icon-corporativo.png">Coorporativo</li>
+								<li><img src="images/icon-debutante.png">Debutante</li>
+								<li><img src="images/icon-formatura.png">Formatura</li>
+							</ul>
+							<ul class="list-events right">
+								<li><img src="images/icon-casamento.png">Casamento</li>
+								<li><img src="images/icon-batizado.png">Batizado</li>
+								<li><img src="images/icon-bar.png">Bar-Mitzvah</li>
+								<li><img src="images/icon-lancamentos.png">Lançamentos imobilários</li>
+							</ul>
+						</div>						
+					</div>
+					<div id="pacotes">
+					<div class="container-textos">
+						<p class="pct-01">Nosso pacote mínimo de serviço consiste em:</p>
+						<ul class="list-pct">
+							<li><i><img src="images/icon1-pacotes.png" /></i>Pedido de 06 caixas com 28 picolés tradicionais equivalentes a 168 picolés.</li>
+							<li><i><img src="images/icon2-pacotes.png" /></i>Fornecimento de 01 carrinho, com guarda-sol e buzininha.</li>
+							<li><i><img src="images/icon3-pacotes.png" /></i>Fornecimento de gelo seco ou placas gelo-gel suficiente para conservação dos picolés por 06 horas.</li>
+							<li><i><img src="images/icon4-pacotes.png" /></i>Entrega e retirada do equipamento no local do evento em horário comercial.</li>
+						</ul>
+					</div>					
+					</div>						
+						
+					<div id="personalizados">
+						<div class="persona-01">
+							<p>Quer personalizar o seu evento? </p>
+							<p>Fazer uma ação de palitos premiado? </p>
+							<p>Divulgar um novo produto?  </p>
+							<p>Ou até mesmo dar desconto através do picolé?  </p>
+						</div>
+						<div class="persona-02">
+							<p>Inove em sua comunicação a Rochinha abre a possibilidade 
+								<br>de trabalhar com picolés personalizados, nosso 
+								<br>pedido mínimo é de 1.500 unidades.</p> 
+	
+							<p>Tenha o picolé e carrinho da sua marca com a qualidade
+								<br>dos produtos Rochinha e desenvolva ações diferenciadas 
+								<br>no mercado, surpreenda. </p>
+						</div>						
+					</div>
+					<div id="sabores">
+						<div class="sab-01">
+							<p>Não deixe também de ter Caipilé 
+							<br>Rochinha nos seus eventos. 
+							<br>Deliciosos sorvetes que viram drinks,
+							que viram convidados satisfeitos. </p>
+						</div>
+												
+					</div>
+					<div id="duvidas"> 
+						<div class="duv-01">
+							<div class="duv-column-01">
+								<span class="duvidas-perguntas">1. Quantos sabores posso escolher? </span>
+								<p class="duvidas-respostas">Os sabores variam de acordo com a quantidade de caixas que irá adquirir, a mesma é lacrada com 28 unidades de um único sabor. 
+								Ex.:  pacote mínimo 06 caixas: 02 coco branco / 01 Limão/ 01 chocolate/01 banana/ 01 milho verde. </p>
+								
+								<span class="duvidas-perguntas">2. Como é feita a entrega e retirada: </span>
+								<p class="duvidas-respostas">Entregamos os picolés e carrinho no local do evento e retiramos após o término, nosso horário de trabalho de segunda a sábado das 08hs as 18h00hs. </p> 
+								
+								<span class="duvidas-perguntas">3. Qual a forma de pagamento? </span>
+								<p class="duvidas-respostas">Sempre antecipado até 72h antes da festa através de boleto bancário.  </p>
+								
+								<span class="duvidas-perguntas">4. Quantos dias de antecedência tenho que fechar o evento? </span>
+								<p class="duvidas-respostas">Solicitamos que o mesmo seja fechado com no mínimo 05 dias úteis de antecedência, após este período fica a cargo de disponibilidade de agenda.  </p>
+								
+								<span class="duvidas-perguntas">5. O carrinho é pago a parte?  </span>
+								<p class="duvidas-respostas">Não, o mesmo compõe o pacote de festas.  </p>
+							</div>
+							<div class="duv-column-02">							
+								<span class="duvidas-perguntas">6. Posso comprar somente os picolés?  </span>
+								<p class="duvidas-respostas">Sim, entregamos o mesmo no local da festa.  </p>
+								
+								<span class="duvidas-perguntas">7. Há cobrança de frete?  </span>
+								<p class="duvidas-respostas">O frete já estará incluso no orçamento passado pelo distribuidor local.  </p>
+								
+								<span class="duvidas-perguntas">8. Vocês tem serviço de “sorveteiro”?  </span>
+								<p class="duvidas-respostas">Não indicamos uma agência que disponibiliza promotores, consultar o atendimento para maiores informações. </p>
+								 
+								<span class="duvidas-perguntas">9. Não quero carrinho. Vocês disponibilizam freezer?  </span>
+								<p class="duvidas-respostas">Sim, pedido mínimo de 15 caixas de picolé, solicite o orçamento.  </p>
+								
+								<span class="duvidas-perguntas">10. Quantos picolés cabem em um carrinho?  </span>
+								<p class="duvidas-respostas">De 07 á 13 caixas dependendo do modelo do equipamento, temos duas variações.  </p>
+								
+								<span class="duvidas-perguntas">11. O que faço com os picolés caso sobre?  </span>
+								<p class="duvidas-respostas">Não trabalhamos com produtos consignados, nossa equipe só retira o carrinho, os picolés ficam por conta do cliente.  </p>
+							</div>
+						</div>						
+						
+					</div>
+					<div id="pedidos">
+						<div class="ped-01">
+						<h3>Para mais informações e orçamento, preencha os dados abaixo.</h3>
+							<form method="post">
+								<label for="nome">Nome<input type="text" name="nome" /></label>
+								<label for="telephone">Telefone<input type="tel" name="telephone" /></label>
+								<br>
+								
+								<label for="email">E-mail <input type="email" name="email" /></label>
+								<label for="event-type">Tipo de evento <input type="text" name="event-type" /></label>
+								<br>
+								
+								<label for="event-local">Local do evento <input type="text" name="event-local" /></label>
+								<label for="event-date">Data do evento <input type="text" name="event-date" /></label>								
+								<label for="convidados">No. de convidados <input type="text" name="convidados" /></label>
+								<br>
+								
+								<label for="msg">Mensagem</label>
+								<textarea name="msg"> </textarea>
+								<input type="submit" value="Enviar mensagem">
+							</form>
+						</div>
+						<div class="footer">
+							<a href="#eventos">Eventos</a> | 
+							<a href="#pacotes">Pacotes</a> |
+							<a href="#personalizados">Personalizados</a> |
+							<a href="#sabores">Sabores</a> |
+							<a href="#duvidas">Duvidas</a> |
+							<a href="#pedidos">Pedidos</a>
+							<img src="images/logo-rodape.png" alt="Sorvetes Rochinha">
+						</div>						
+					</div>					
+				</div>
+			</div>	
+		</div>		
+		<script src="js/jquery-2.1.0.min.js"></script>
+		<script src="js/bootstrap.min.js"></script>
+		<script src="js/main.js"></script>
+	</body>
+</html>
